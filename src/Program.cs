4beff837f33f6812f6e42d4267c2efe70ee38361
Daniel Heise <daniel.heise@5minds.de>
2019-04-26 17:46:59 +0200

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeSafeEnumRef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
